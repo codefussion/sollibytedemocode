bf5295fc1a9e947544d1390b939970b3e1375f10
AndyOkuba <andyokuba@gmail.com>
2021-05-14 15:38:27 +0300

    first commit

--- src/main/java/com/solidarity/SolidarityApplication.java
+++ src/main/java/com/solidarity/SolidarityApplication.java
@@ -2,12 +2,20 @@ package com.solidarity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-public class SolidarityApplication {
+public class SolidarityApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder app) { 
+	  return app.sources( SolidarityApplication.class); 
+	}
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SolidarityApplication.class, args);
 	}
